37154c2ad469a3228bcabc8e6e52be10a1477b3a
fireowl0013 <fireowl0013@proton.me>
2023-04-17 13:44:31 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+UUID='a3b9b6f2-208c-47aa-9843-f0e8ff6c2f6a'
+VMESS_FIREOWL='/vmess'
+VLESS_FIREOWL='/vless'
+TROJAN_FIREOWL='/trojan'
+SS_FIREOWL='/shadowsocks'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
